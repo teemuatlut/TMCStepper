66c75bec31102a34731eb3113ba1ec4974ad0974
teemuatlut <teemu.mantykallio@live.fi>
2019-05-16 11:51:26 +0300

    WIP len can be const

--- src/source/TMC2208Stepper.cpp
+++ src/source/TMC2208Stepper.cpp
@@ -74,7 +74,7 @@ void TMC2208Stepper::write(uint8_t addr, uint32_t regVal) {
 }
 
 template<typename SERIAL_TYPE>
-uint64_t _sendDatagram(SERIAL_TYPE &serPtr, uint8_t datagram[], uint8_t len, uint16_t timeout) {
+uint64_t _sendDatagram(SERIAL_TYPE &serPtr, uint8_t datagram[], const uint8_t len, uint16_t timeout) {
 	while (serPtr.available() > 0) serPtr.read(); // Flush
 	for(int i=0; i<=len; i++) serPtr.write(datagram[i]);
 
