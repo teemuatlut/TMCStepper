389276ac499f57fd2c9b178ff5cba98b6efbdc35
teemuatlut <teemu.mantykallio@live.fi>
2019-05-30 00:34:02 +0300

    Slightly reduce DUE SW SPI frequency

--- src/source/TMC_platforms.h
+++ src/source/TMC_platforms.h
@@ -7,13 +7,13 @@
   #define writeSCK_H *sck_register |= sck_bm
   #define writeSCK_L *sck_register &= ~sck_bm
   #define readMISO *miso_register & miso_bm
-#elif defined(ARDUINO_ARCH_SAM) // DUE:1.2MHz
+#elif defined(ARDUINO_ARCH_SAM) // DUE:700kHz
   // by stimmer https://forum.arduino.cc/index.php?topic=129868.msg980466#msg980466
   #define writeMOSI_H g_APinDescription[mosi_pin].pPort -> PIO_SODR = g_APinDescription[mosi_pin].ulPin
   #define writeMOSI_L g_APinDescription[mosi_pin].pPort -> PIO_CODR = g_APinDescription[mosi_pin].ulPin
   #define writeSCK_H g_APinDescription[sck_pin].pPort -> PIO_SODR = g_APinDescription[sck_pin].ulPin
   #define writeSCK_L g_APinDescription[sck_pin].pPort -> PIO_CODR = g_APinDescription[sck_pin].ulPin
-  #define readMISO !!(g_APinDescription[miso_pin].pPort -> PIO_PDSR & g_APinDescription[miso_pin].ulPin)
+  #define readMISO PIO_Get( g_APinDescription[miso_pin].pPort, PIO_INPUT, g_APinDescription[miso_pin].ulPin )
 #elif defined(TARGET_LPC1768) // LPC1769:2.4MHz
   typedef volatile LPC_GPIO_TypeDef* fastio_reg;
   typedef uint32_t fastio_bm;
