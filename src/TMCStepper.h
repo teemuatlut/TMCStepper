fd2216e4d407ded510665251f4129f5b474cebf3
teemuatlut <teemu.mantykallio@live.fi>
2019-06-17 17:25:49 +0300

    v0.4.3

--- src/TMCStepper.h
+++ src/TMCStepper.h
@@ -32,7 +32,7 @@
 #define INIT2224_REGISTER(REG) TMC2224_n::REG##_t REG##_register = TMC2224_n::REG##_t
 #define SET_ALIAS(TYPE, DRIVER, NEW, ARG, OLD) TYPE (DRIVER::*NEW)(ARG) = &DRIVER::OLD
 
-#define TMCSTEPPER_VERSION 0x000402 // v0.4.2
+#define TMCSTEPPER_VERSION 0x000403 // v0.4.3
 
 class TMCStepper {
 	public:
