c181e7e84defb2fef6eaf19bdca9c7b79db1cff6
Teemu Mäntykallio <teemu.mantykallio@live.fi>
2020-05-31 12:33:41 +0300

    HAL AVR

---
+++ src/source/TMC_HAL.h
@@ -0,0 +1,54 @@
+
+#pragma once
+
+#if defined(ARDUINO_ARCH_AVR)
+
+    namespace TMCStepper_n {
+        typedef uint8_t PinDef;
+
+        class TMCPin {
+        public:
+            explicit TMCPin(const uint8_t _pin);
+            void mode(const uint8_t mode) const;
+            bool read() const;
+            operator bool() const {
+                return read();
+            }
+
+        protected:
+            volatile uint8_t* const port = nullptr;
+            uint8_t const bitMask;
+        };
+
+        class OutputPin : public TMCPin {
+        public:
+            OutputPin(const uint8_t _pin);
+            void write(const bool state) const;
+        };
+
+        typedef TMCPin InputPin;
+    }
+
+#endif
+
+#ifndef HIGH
+    #define HIGH 1
+#endif
+#ifndef LOW
+    #define LOW 0
+#endif
+#ifndef INPUT
+    #define INPUT  0x00
+#endif
+#ifndef INPUT_PULLUP
+    #define INPUT_PULLUP  0x01
+#endif
+#ifndef OUTPUT
+    #define OUTPUT 0x01
+#endif
+#if !defined(MSBFIRST)
+    #define MSBFIRST 1
+#endif
+#ifndef SPI_MODE3
+    #define SPI_MODE3 0
+#endif
---
+++ src/source/TMC_HAL/TMC_HAL_AVR.cpp
@@ -0,0 +1,170 @@
+
+#if defined(ARDUINO_ARCH_AVR)
+
+#include <Arduino.h>
+#include <SPI.h>
+#include "../TMC_HAL.h"
+#include "TMCStepper.h"
+
+using namespace TMCStepper_n;
+
+TMCPin::TMCPin(const uint8_t _pin) :
+    port(portOutputRegister(digitalPinToPort(_pin))),
+    bitMask(digitalPinToBitMask(_pin))
+    {}
+
+void TMCPin::mode(const uint8_t mode) const {
+    volatile uint8_t *reg = portModeRegister(*port);
+    volatile uint8_t *out = portOutputRegister(*port);
+
+    uint8_t oldSREG = SREG;
+    cli();
+
+    switch(mode) {
+        case OUTPUT:
+            *reg |= bitMask;
+            break;
+        case INPUT:
+            *reg &= ~bitMask;
+            *out &= ~bitMask;
+            break;
+        default: break;
+    }
+    SREG = oldSREG;
+}
+
+bool TMCPin::read() const {
+    return *port & bitMask;
+}
+
+OutputPin::OutputPin(const uint8_t _pin) : TMCPin(_pin) {}
+
+void OutputPin::write(const bool state) const {
+    if (state)
+        *port |= bitMask;
+    else
+        *port &= ~bitMask;
+}
+
+__attribute__((weak))
+void TMC2130Stepper::beginTransaction() {
+    if (TMC_HW_SPI != nullptr) {
+        TMC_HW_SPI->beginTransaction(SPISettings(spi_speed, MSBFIRST, SPI_MODE3));
+    }
+}
+
+__attribute__((weak))
+void TMC2130Stepper::transfer(char *buf, const uint8_t count) {
+    if(TMC_HW_SPI != nullptr) {
+        TMC_HW_SPI->transfer(buf, count);
+    }
+    else if(TMC_SW_SPI != nullptr) {
+        TMC_SW_SPI->transfer(buf, count);
+    }
+}
+
+__attribute__((weak))
+void TMC2130Stepper::endTransaction() {
+    if (TMC_HW_SPI != nullptr) {
+        TMC_HW_SPI->endTransaction();
+    }
+}
+
+__attribute__((weak))
+void TMC2660Stepper::beginTransaction() {
+    if (TMC_HW_SPI != nullptr) {
+        TMC_HW_SPI->beginTransaction(SPISettings(spi_speed, MSBFIRST, SPI_MODE3));
+    }
+}
+
+__attribute__((weak))
+void TMC2660Stepper::transfer(char *buf, const uint8_t count) {
+    if(TMC_HW_SPI != nullptr) {
+        TMC_HW_SPI->transfer(buf, count);
+    }
+}
+
+__attribute__((weak))
+void TMC2660Stepper::endTransaction() {
+    if (TMC_HW_SPI != nullptr) {
+        TMC_HW_SPI->endTransaction();
+    }
+}
+
+__attribute__((weak))
+int TMC2208Stepper::available() {
+    int out = 0;
+    if (SWSerial != nullptr) {
+        out = SWSerial->available();
+    } else if (HWSerial != nullptr) {
+        out = HWSerial->available();
+    }
+
+    return out;
+}
+
+__attribute__((weak))
+size_t TMC2208Stepper::getTime() const {
+    return millis();
+}
+
+__attribute__((weak))
+void TMC2208Stepper::preWriteCommunication() {
+    if (HWSerial != nullptr) {
+        if (sswitch != nullptr)
+            sswitch->active();
+    }
+}
+
+__attribute__((weak))
+void TMC2208Stepper::preReadCommunication() {
+    #if SW_CAPABLE_PLATFORM
+        if (SWSerial != nullptr) {
+            SWSerial->listen();
+        } else
+    #endif
+        if (HWSerial != nullptr) {
+            if (sswitch != nullptr)
+                sswitch->active();
+        }
+}
+
+__attribute__((weak))
+void TMC2208Stepper::serial_read(uint8_t *data, int8_t length) {
+    if (RXTX_pin > 0) {
+        pinMode(RXTX_pin, INPUT_PULLUP);
+    }
+
+    if (SWSerial != nullptr && SWSerial->available() > 0) {
+        SWSerial->readBytes(data, length);
+    } else if (HWSerial != nullptr && HWSerial->available() > 0) {
+        HWSerial->readBytes(data, length);
+    }
+}
+
+__attribute__((weak))
+void TMC2208Stepper::serial_write(const uint8_t *data, int8_t length) {
+    if (RXTX_pin > 0) {
+        digitalWrite(RXTX_pin, HIGH);
+        pinMode(RXTX_pin, OUTPUT);
+    }
+
+    if (SWSerial != nullptr) {
+        SWSerial->write(data, length);
+    }
+    else if (HWSerial != nullptr) {
+        HWSerial->write(data, length);
+    }
+}
+
+__attribute__((weak))
+void TMC2208Stepper::postWriteCommunication() {}
+
+__attribute__((weak))
+void TMC2208Stepper::postReadCommunication() {
+        if (SWSerial != nullptr) {
+            SWSerial->end();
+        }
+}
+
+#endif
