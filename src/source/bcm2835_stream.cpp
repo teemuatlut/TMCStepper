1b07050e21d9df5a5ba3275217eba901095437ea
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2020-09-28 18:42:40 +0200

    Update bcm2835_stream.cpp

--- src/source/bcm2835_stream.cpp
+++ src/source/bcm2835_stream.cpp
@@ -1,3 +1,8 @@
+#if defined(bcm2835)
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "bcm2835_stream.h"
 
 uint32_t millis()
@@ -7,17 +12,62 @@ uint32_t millis()
 	return (uint32_t)now.tv_usec;
 }
 
+Stream::Stream(const char* port)
+{
+	Stream::port = port;
+}
+
+void Stream::begin(unsigned long baud, int flags)
+{
+	struct termios options;
+
+	fd = open(port, flags);
+	if (fd == -1) {
+		printf("[ERROR] UART open(%s)\n", port);
+		return;
+	}
+	fcntl(fd, F_SETFL, O_RDWR);
+
+	tcgetattr(fd, &options);
+	options.c_cflag = CS8 | CLOCAL | CREAD | CBAUDEX;
+	options.c_cflag &= ~CBAUD;
+	options.c_iflag = IGNPAR;
+	options.c_oflag = baud;
+	options.c_lflag = baud;
+	tcflush(fd, TCIFLUSH);
+	tcsetattr(fd, TCSANOW, &options);
+}
+
+void Stream::end()
+{
+	::close(fd);
+}
+
 int Stream::available()
 {
-    return 0;
+	if (fd != -1)
+	{
+		int result;
+		if (ioctl(fd, FIONREAD, &result) == -1)
+			return -1;
+		return result;
+	}
 }
 
 uint8_t Stream::write(const uint8_t data)
 {
-    return uint8_t();
+    return (uint8_t)::write(fd, &data, 1);
 }
 
 uint8_t Stream::read()
 {
-    return uint8_t();
+	uint8_t data = -1;
+	if (::read(fd, &data, 1) == -1)
+		return -1;
+	return data;
 }
+
+Stream Serial("/dev/serial0");
+Stream Serial1("/dev/serial1");
+
+#endif
