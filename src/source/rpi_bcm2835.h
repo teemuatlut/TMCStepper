a5b666e3d9f8c8ec424f14fcf4da757c165b081e
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2020-09-28 15:04:06 +0200

    Add Raspberry Pi (3B+) as MCU
    define bcm2835 to use it

--- src/source/rpi_bcm2835.h
+++ src/source/rpi_bcm2835.h
@@ -1,4 +1,6 @@
 #pragma once
+#if defined(bcm2835)
+
 #include <bcm2835.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -10,3 +12,5 @@
 #define pinMode(PIN, MODE) bcm2835_gpio_fsel(PIN, MODE); if (MODE == (uint8_t)BCM2835_GPIO_PUD_UP || MODE == (uint8_t)BCM2835_GPIO_PUD_DOWN) bcm2835_gpio_set_pud(PIN, MODE)
 #define digitalWrite(PIN, MODE) bcm2835_gpio_write(PIN, MODE)
 #define digitalRead(PIN)  bcm2835_gpio_lev(PIN)
+
+#endif
