a52d85866b514890fe17af8835dbdeb81b319b5f
Teemu Mäntykallio <teemu.mantykallio@live.fi>
2021-05-05 19:04:33 +0300

    Add missing definition for TMC2209Stepper::begin

--- src/source/TMC2209Stepper.cpp
+++ src/source/TMC2209Stepper.cpp
@@ -16,6 +16,14 @@ TMC2209Stepper::TMC2209Stepper(HardwareSerial &SerialPort, float RS, uint8_t add
 		}
 #endif
 
+void TMC2209Stepper::begin() {
+	#if SW_CAPABLE_PLATFORM
+		beginSerial(19200);
+	#endif
+	pdn_disable(true);
+	mstep_reg_select(true);
+}
+
 void TMC2209Stepper::defaults() {
 	GCONF(1 | (1<<8));
 	SLAVECONF(0);
